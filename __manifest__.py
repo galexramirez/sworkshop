9d1760775fc64c455ee7bd76dc338d987f539bd3
AlexDev <galexramirez@yahoo.com>
2024-05-23 03:54:56 +0000

    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sworkshop",
-
+    'name': "Service Workshop",
+    
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
     'description': """
 Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Alexander Ramirez",
+    'website': "https://www.conectiva.com.pe",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -30,5 +30,7 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    
+    'license': 'LGPL-3'
 }
 
